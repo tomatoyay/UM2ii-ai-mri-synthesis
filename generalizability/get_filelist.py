631c2babbb194c16e90a234cab24ab08c04831c6
Vivian Zhang <59489753+tomatoyay@users.noreply.github.com>
2022-09-15 19:22:09 -0400

    Create get_filelist.py

---
+++ generalizability/get_filelist.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 19:17:57 2022
+
+@author: vivianzhang
+"""
+#imports
+
+import numpy as np
+import os
+import pydicom
+import pandas as pd
+from pathlib import Path
+
+
+#%%
+# Insert parent directory's path
+path = 'insert path here'
+
+#assign labels
+def label_folder(parent, label_dict):
+    # iterate over all the files in directory 'parent'
+    for file_name in os.listdir(parent):
+        if file_name.endswith('.dcm'):
+            #get file and file path
+            file = pydicom.data.data_manager.get_files(parent,file_name)[0]
+            file_path = "".join((parent, "/", file_name))
+            
+            #split file path into specific names
+            names = file_path.split('/')
+            hospital = names[6]
+            patient = names[6]
+            study = names[7]\
+            
+            # if you need to actually read the dicom
+            ds = pydicom.dcmread(file)
+            description = ds.SeriesDescription.lower()
+            sex = ds.PatientSex
+
+            #create list of things to put into dataframe
+            labels = [patient, hospital, study, description, sex, birthdate, acq_date]
+            
+            #add to dictionary
+            if file_path not in label_dict:
+                label_dict[file_path] = labels
+            else:
+                return
+        else:
+            #iterate on loop
+            current_path = "".join((parent, "/", file_name))
+            if os.path.isdir(current_path):
+                # if we're checking a sub-directory, recursively call this method
+                label_folder(current_path, label_dict)
+                
+    return label_dict
+
+#%% execute code
+
+#create empty dictionary
+label_dict = dict()
+
+#execute function
+labels = label_folder(path, label_dict)
+
+
+#%% export
+
+#export to dataframe
+df = pd.DataFrame.from_dict(data = label_dict, orient = 'index', columns = ['column titles here',...)
+
+#export to excel file
+#df.to_csv('name.csv', sep=str(','), index = True)
